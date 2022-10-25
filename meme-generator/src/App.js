da9dd2bd37554a1738f39b02d688711cd7a35a61
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-10-25 10:29:44 -0400

    first commit

---
+++ meme-generator/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './style.css'
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;

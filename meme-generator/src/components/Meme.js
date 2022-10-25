e73e42d0c29f5ef32e94eb1339002f2969364d15
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-10-25 14:26:36 -0400

    render images update w/ state

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,9 +1,31 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -16,10 +38,12 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+            <img src={meme.randomImage} alt="" className="meme--image" />
         </main>
     )
 }
